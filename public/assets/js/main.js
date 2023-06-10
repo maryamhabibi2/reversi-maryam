7a4d54b21e2d48543e0c1601a76204fe995cd855
Maryam Habibi <mhabibi2@uci.edu>
2023-06-10 12:41:45 -0700

    Final Game

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -211,7 +211,7 @@ socket.on('player_disconnected',(payload) => {
         domElements.hide("fade", 500);
     }
 
-    let newHTML = '<p class =\'left_room_response\'>'+payload.username+ ' left the '+payload.room+'.(There are '+payload.count+' users in this room)</p>';
+    let newHTML = '<p class =\'left_room_response\'>'+payload.username+ ' left the chatroom. (There are ' + payload.count + ' users in this room)</p>';
     let newNode = $(newHTML);
     newNode.hide();
     $('#messages').prepend(newNode);
@@ -244,17 +244,18 @@ socket.on('send_chat_message_response',(payload) =>{
 })
 
 let old_board = [
-    ['?','?','?','?','?','?','?','?',],
-    ['?','?','?','?','?','?','?','?',],
-    ['?','?','?','?','?','?','?','?',],
-    ['?','?','?','?','?','?','?','?',],
-    ['?','?','?','?','?','?','?','?',],
-    ['?','?','?','?','?','?','?','?',],
-    ['?','?','?','?','?','?','?','?',],
-    ['?','?','?','?','?','?','?','?',]
+    [' ',' ',' ',' ',' ',' ',' ',' ',],
+    [' ',' ',' ',' ',' ',' ',' ',' ',],
+    [' ',' ',' ',' ',' ',' ',' ',' ',],
+    [' ',' ',' ',' ',' ',' ',' ',' ',],
+    [' ',' ',' ',' ',' ',' ',' ',' ',],
+    [' ',' ',' ',' ',' ',' ',' ',' ',],
+    [' ',' ',' ',' ',' ',' ',' ',' ',],
+    [' ',' ',' ',' ',' ',' ',' ',' ',]
 ];
 
 let my_color = "";
+let interval_timer;
 
 socket.on('game_update',(payload) =>{
     if(( typeof payload == 'undefined') || (payload === null)){
@@ -285,8 +286,28 @@ socket.on('game_update',(payload) =>{
         window.location.href= 'lobby.html?username=' + username;
         return;
     }
+    
+    if(my_color === 'white'){
+        $("#my_color").html('<h3 id="my_color">I am white</h3>');
 
-    $("#my_color").html('<h3 id="my_color">I am ' +my_color + '</h3>');
+    }
+    else if( my_color === 'black'){
+        $("#my_color").html('<h3 id="my_color">I am black</h3>');
+    }
+    else{
+        $("#my_color").html('<h3 id="my_color">Error: I don\'t know what color I am</h3>');    
+    }
+
+    if(payload.game.whose_turn === 'white'){
+        $("#my_color").append('<h4>It is white\'s turn</h4>');
+
+    }
+    else if( payload.game.whose_turn === 'black'){
+        $("#my_color").append('<h4>It is black\'s turn</h4>');    
+    }
+    else{
+        $("#my_color").append('<h4>Error: Don\'t know whose turn it is</h4>');    
+    }
 
     let whitesum = 0 
     let blacksum = 0 
@@ -301,7 +322,7 @@ socket.on('game_update',(payload) =>{
                 blacksum++;
             }
 
-            /*Check to see if the server changed any spaceon the board */
+            /*Check to see if the server changed any space on the board */
             if (old_board[row][column] !== board[row][column]) {
                 let graphic = "";
                 let altTag = "";
@@ -347,28 +368,55 @@ socket.on('game_update',(payload) =>{
                 }
                 const t = Date.now();
                 $('#' + row + '_'+column).html('<img class="img-fluid" src="assets/Images/' + graphic + '?time=' + t + '" alt="' + altTag + '" />'); 
-               
-                $('#' + row + '_'+column).off('click');
-                if (board[row][column] === ' '){
-                    $('#' + row + '_'+column).addClass('hovered_over');
-                    $('#' + row + '_'+column).click(((r,c) => {
-                        return (() => {
-                            let payload = {
-                                row: r,
-                                column:c,
-                                color: my_color
-                            };
-                            console.log('**** client log message, sneding \'play_token\' command:'+JSON.stringify(payload));
-                            socket.emit('play_token', payload);
-                        });
+            }
+            /* Set up interactivity */
+            $('#' + row + '_'+column).off('click');
+            $('#' + row + '_'+column).removeClass('hovered_over');
+            if(payload.game.whose_turn === my_color){
+                if(payload.game.legal_moves[row][column] === my_color.substr(0,1)){
+                $('#' + row + '_'+column).addClass('hovered_over');
+                $('#' + row + '_'+column).click(((r,c) => {
+                    return (() => {
+                        let payload = {
+                            row: r,
+                            column:c,
+                            color: my_color
+                        };
+                        console.log('**** client log message, sneding \'play_token\' command:'+JSON.stringify(payload));
+                        socket.emit('play_token', payload);
+                    });
                     })(row, column));
-                }
-                else{
-                    $('#' + row + '_'+column).removeClass('hovered_over');
                 }
             }
         }
-    }   
+    }
+    
+    clearInterval(interval_timer)
+    interval_timer = setInterval( ((last_time) => {
+        return ( () =>{
+            let d = new Date();
+            let elapsed_m = d.getTime() - last_time;
+            let minutes = Math.floor((elapsed_m/1000)/ 60);
+            let seconds = Math.floor((elapsed_m % (60 * 1000))/ 1000);
+            let total = minutes * 60 + seconds;
+            if (total > 100){
+                total = 100;
+            }
+            $("#elapsed").css("width",total+"%").attr("aria-vlauenow",total);
+            let timestring = "" +seconds;
+            timestring = timestring.padStart(2,'0');
+            timestring = minutes + ":" + timestring;
+            if (total < 100){
+                $("#elapsed").html(timestring);
+            }
+            else {
+                $("#elapsed").html("Times up!");
+            }
+        })
+    })(payload.game.last_move_time)
+        , 1000);
+
+
     $("#whitesum").html(whitesum);
     $("#blacksum").html(blacksum);
     old_board = board;
@@ -381,6 +429,7 @@ socket.on('play_token_response',(payload) =>{
     }
     if(payload.result === 'fail'){
         console.log(payload.message);
+        alert(payload.message)
         return;
     }
 })
